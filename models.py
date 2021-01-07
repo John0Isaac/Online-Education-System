524b0c75d8befb8e35991e9da3f3899a3c7330cd
John0Isaac <johnaziz269@gmail.com>
2021-01-08 00:01:28 +0200

    Student Class Model

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from operator import add
 from sqlalchemy import Column, String, Integer, Boolean, ForeignKey, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
@@ -18,3 +19,61 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+'''
+Student
+
+'''
+
+
+class Student(db.Model):
+    ___tablename__ = 'student'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String(120))
+    address = Column(String)
+    phone = Column(String(120))
+    gender = Column(String)
+    national_id = Column(String)
+    date_of_birth = Column(String)
+    logincode = Column(String)
+    specialization_id = Column(Integer, ForeignKey('specialization.id'), nullable=False)
+    section_id = Column(Integer, ForeignKey('section.id'), nullable=False)
+
+    def __init__(self, name, email, address, phone, gender, national_id, date_of_birth, logincode, specialization_id, section_id):
+        self.name = name
+        self.email = email
+        self.address = address
+        self.phone = phone
+        self.gender = gender
+        self.national_id = national_id
+        self.date_of_birth = date_of_birth
+        self.logincode = logincode
+        self.specialization_id = specialization_id
+        self.section_id = section_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'address': self.address,
+            'phone': self.phone,
+            'gender': self.gender,
+            'national_id': self.national_id,
+            'date_of_birth': self.date_of_birth,
+            'logincode': self.logincode,
+            'specialization_id': self.specialization_id,
+            'section_id': self.section_id
+        }
