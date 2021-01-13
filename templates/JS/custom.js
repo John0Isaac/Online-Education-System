53c60823c74643a961becc89a2c86228f3b0e325
Rana Agha <71423885+ranaagha5@users.noreply.github.com>
2021-01-13 22:55:05 +0200

    Update custom.js

--- templates/JS/custom.js
+++ templates/JS/custom.js
@@ -1,10 +1,17 @@
-//jQuery(document).ready(function(){
-//    let show = document.getElementById('btnshow');
-//    let list = document.getElementsByClassName('list')[0];
-//    show.addEventListener('click',function(){
-//     if (list.style.display =="block"){
-//         list.style.display="none";
-//     } else{
-//         list.style.display="block";
-//     }
-//    })};
+// the selector will match all input controls of type :checkbox
+// and attach a click event handler 
+$("input:checkbox").on('click', function() {
+  // in the handler, 'this' refers to the box clicked on
+  var $box = $(this);
+  if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+  } else {
+    $box.prop("checked", false);
+  }
+});
