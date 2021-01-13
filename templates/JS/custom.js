2d6dad491035375227fa5dba77165066c9ba634e
ranaagha5 <ranaagha50@gmail.com>
2021-01-13 22:28:47 +0200

    front end

---
+++ templates/JS/custom.js
@@ -0,0 +1,10 @@
+//jQuery(document).ready(function(){
+//    let show = document.getElementById('btnshow');
+//    let list = document.getElementsByClassName('list')[0];
+//    show.addEventListener('click',function(){
+//     if (list.style.display =="block"){
+//         list.style.display="none";
+//     } else{
+//         list.style.display="block";
+//     }
+//    })};
